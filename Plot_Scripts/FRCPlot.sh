ee9e489b65236566916b6ea8cd4b1ef788f34fc7
Mingxu Hu <humingxu@gmail.com>
2015-08-22 17:27:21 +0800

    Move File

--- Plot_Scripts/FRCPlot.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-python FRC_ScatterPlot.py ../Data/Li_Raw_FRC.txt ../Data/Li_Corrected_FRC.txt \
-    ../Figures/Li_FRC.png Frequency FRC
---
+++ Plot_Scripts/FRC_ScatterPlot_CompareTwo.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+python FRC_ScatterPlot_CompareTwo.py \
+    ../Data/FRC_stack_1308_2x_Corrected_Base.txt \
+    ../Data/FRC_stack_1308_2x_Corrected_14.txt \
+    ../Figures/FRC_stack_1308_2x_Corrected.png Frequency FRC
