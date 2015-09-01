ebe9724cf1e7b53ae10bc5c8d84bb7029d7963b7
Mingxu Hu <humingxu@gmail.com>
2015-09-01 20:00:14 +0800

    FRCFittingCompareApp Added

---
+++ Calculation/stack_1308_FRCCompare.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+python FRCFittingCompareApp.py ../Data/RawFRC.txt \
+    ../Data/CorrectedFRC.txt 3838 17963 18713 0.0196 \
+    1.32 0 1350 180 1300
---
+++ Calculation/stack_1308_RawFRCFitting.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+python FRCFittingApp.py ../Data/RawFRC.txt  3838 17963 18713 0.0196 \
+    1.32 0 1350 180 1300
