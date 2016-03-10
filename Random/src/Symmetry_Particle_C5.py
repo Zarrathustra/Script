0ba76b677b2840d685c086c08852d89564f1d1e7
Mingxu Hu <humingxu@gmail.com>
2016-03-10 23:28:24 +0800

    Particle_C5 Added

---
+++ Random/src/Symmetry_Particle_C5.py
@@ -0,0 +1,27 @@
+from pylab import *
+
+N = 6000
+
+filename = "../Data/Particle_C5.txt"
+
+lines = open(filename, "r").readlines();
+
+X = [float(line.strip().split()[0]) for line in lines]
+Y = [float(line.strip().split()[1]) for line in lines]
+
+figure(figsize = (20, 10), dpi = 500)
+
+scatter(X[:N], Y[:N], alpha = 0.5, linewidth = 0, color = "black")
+
+xlim(-0.5, 2 * pi + 0.5)
+ylim(-0.5, pi + 0.5)
+
+xticks([0, 2 * pi / 5, pi, 2 * pi], ["0", r"$\frac{2\pi}{5}$", r"$\pi$", r"$2\pi$"])
+yticks([0, pi], ["0", r"$\pi$"])
+
+xlabel(r"$\phi$")
+ylabel(r"$\theta$")
+
+title(r"$\phi$ and $\theta$ of Uniform Distribution of C5 Symmetry Sphere")
+
+savefig("../Figures/Phi_Theta_Symmetry_C5.png", dpi = 500)
